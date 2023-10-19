e4ff1ad3053cea60c1b9b193e285de3e2f54660a
Yokk <thanawat.yokk@gmail.com>
2023-10-19 23:02:27 +0700

    Add backend and frontend workspace

---
+++ build-up-backend/src/main/java/com/gemini11/buildupbackend/BuildUpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gemini11.buildupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildUpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildUpBackendApplication.class, args);
+	}
+
+}
