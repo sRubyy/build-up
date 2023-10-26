58c1f371e4ca8270f4933b4d0504249076b67957
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-26 10:57:37 +0700

    prettier code in ./build-up-frontend

--- build-up-frontend/src/App.js
+++ build-up-frontend/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Outlet } from "react-router-dom";
-import './app.scss'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import './app.scss';
 
 function App() {
   return (
     <div>
-      <div className='global-nav'>Nav</div>
+      <div className="global-nav">Nav</div>
       <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
