70fb970804fe4a95f09af562df5de36822523e95
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-26 10:57:37 +0700

    prettier code in ./build-up-frontend

--- build-up-frontend/src/pages/HomePage.jsx
+++ build-up-frontend/src/pages/HomePage.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function HomePage() {
-  return (
-    <div>Home Page</div>
-  )
+  return <div>Home Page</div>;
 }
 
-export default HomePage
+export default HomePage;
