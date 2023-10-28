9d742d8823491a2aed6439a99a3e77fefa7fabf3
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-29 03:26:33 +0700

    draft search bar in homepage

--- build-up-frontend/src/components/global/Navbar.jsx
+++ build-up-frontend/src/components/global/Navbar.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import '../../scss/global/navbar.scss';
 
 function Navbar() {
-  return <div className="global-nav">Buildup</div>;
+  return (
+    <div className="global-nav">
+      <div className="global-nav__left global-nav__left--branding-text">
+        Buildup
+      </div>
+      <div className="global-nav__center">
+        <input
+          className="global-nav__input"
+          type="text"
+          placeholder="search your style here"
+        />
+        <div className="global-nav__search-icon"></div>
+      </div>
+      <div className="global-nav__right">
+        <div className="global-nav__cart-icon"></div>
+        <div>Sign up</div>
+        <div>Log in</div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
