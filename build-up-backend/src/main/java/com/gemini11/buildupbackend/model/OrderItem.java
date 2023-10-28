847718407d189096eb025ee889eb10d18456f32e
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-28 12:50:23 +0700

    [GEN-59] refactor code

--- build-up-backend/src/main/java/com/gemini11/buildupbackend/model/OrderItem.java
+++ build-up-backend/src/main/java/com/gemini11/buildupbackend/model/OrderItem.java
@@ -7,23 +7,40 @@ import jakarta.persistence.*;
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer order_item_id;
+    @Column(name = "order_item_id")
+    private Integer orderItemId;
 
     @ManyToOne
-    @JoinColumn(name = "shoe_id")
+    @JoinColumn(name = "product_id")
     private Product product; // Many order items can have one item
 
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order; // Many order items belong to one order
 
-    private Integer item_quantity;
+    private Integer itemQuantity;
 
-    public Integer getOrderItemId() {return order_item_id;}
-    public Product getProduct() {return product;}
-    public Integer getItem_quantity() {return item_quantity;}
-    public Order getOrder() {return order;}
-    public void setShoe(Product product) {this.product = product;}
-    public void setItem_quantity(int item_quantity) {this.item_quantity = item_quantity;}
+    public Product getProduct() {
+        return product;
+    }
 
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Integer getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(Integer itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
 }
--- build-up-backend/src/main/java/com/gemini11/buildupbackend/model/Product.java
+++ build-up-backend/src/main/java/com/gemini11/buildupbackend/model/Product.java
@@ -8,35 +8,86 @@ import java.time.LocalDateTime;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer product_id;
+    @Column(name = "product_id")
+    private Integer productId;
 
     private String name;
+    private String description;
     private Double price;
     private String type;
     private String size;
-    private Boolean brand_new;
-    private String description;
-    private LocalDateTime created_at;
+
+    @Column(name = "is_brand_new")
+    private Boolean isBrandNew;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "seller_id")
     private User seller;
 
-    public Integer getShoeId() {return product_id;}
-    public String getName() {return name;}
-    public Double getPrice() {return price;}
-    public String getType() {return type;}
-    public String getSize() {return size;}
-    public Boolean getBrand_new() {return brand_new;}
-    public String getDescription() {return description;}
-    public LocalDateTime getCreated_at() {return created_at;}
-    public User getSeller() {return seller;}
-    public void setName(String name) {this.name = name;}
-    public void setPrice(Double price) {this.price = price;}
-    public void setSize(String size) {this.size = size;}
-    public void setBrand_new(Boolean brand_new) {this.brand_new = brand_new;}
-    public void setSeller(User seller) {this.seller = seller;}
-    public void setDescription(String description) {this.description = description;}
-    public void setCreated_at(LocalDateTime created_at) {this.created_at = created_at;}
-    public void setType(String type) {this.type = type;}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Boolean getBrandNew() {
+        return isBrandNew;
+    }
+
+    public void setBrandNew(Boolean brandNew) {
+        isBrandNew = brandNew;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
 }
