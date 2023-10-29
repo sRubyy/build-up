db2861e8a8e830f402c7d00921368ccc8d29b4f2
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-29 13:25:41 +0700

    complete homepage upper section

--- build-up-frontend/src/components/homepage/BrandIcon.jsx
+++ build-up-frontend/src/components/homepage/BrandIcon.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../scss/homepage/homepage.scss';
 
-function BrandIcon({ name }) {
+function BrandIcon({ name, logoUrl }) {
   return (
     <div className="brand-slot">
-      <div className="brand-slot__icon"></div>
+      <div className="brand-slot__icon">
+        <img className="brand-slot__icon--img" alt="" src={logoUrl} />
+      </div>
       <div className="brand-slot__font">{name}</div>
     </div>
   );
