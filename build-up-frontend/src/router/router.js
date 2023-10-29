73194f14fd04b5ea18665fea49a6b2f202742dea
mon <mon.ppunnavich@gmail.com>
2023-10-29 15:20:43 +0700

    [GEN-60] rebase with origin/dev

--- build-up-frontend/src/router/router.js
+++ build-up-frontend/src/router/router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import HomePage from '../pages/HomePage';
+import { ProductDescription } from '../pages/ProductDescription/ProductDescription';
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
         path: '/',
         element: <HomePage />,
       },
+      {
+        path: '/productDescription',
+        element: <ProductDescription />,
+      }
     ],
   },
 ]);
