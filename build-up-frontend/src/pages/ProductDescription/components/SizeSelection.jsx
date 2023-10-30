336367d69c99486a91d00f23563f1300fb77dffe
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-30 13:11:26 +0700

    [GEN-33] create AvailableList.jsx

--- build-up-frontend/src/pages/ProductDescription/components/SizeSelection.jsx
+++ build-up-frontend/src/pages/ProductDescription/components/SizeSelection.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../../../scss/product_description/size_selection.scss';
 import { useState } from 'react';
+import { SizePool } from './SizePool';
+import { AvailableList } from './AvailableList';
 
 export const SizeSelection = () => {
   const [isNewType, setIsNewType] = useState(true);
@@ -73,22 +75,8 @@ export const SizeSelection = () => {
           USED
         </div>
       </div>
-      <div className={'select-type__second-desc'}>
-        <div className={'select-type__size-desc'}>Size (US Unisex)</div>
-        <div className={'select-type__size-chart'}>Size Chart</div>
-      </div>
-      <div className={'select-type__size-pool'}>
-        {Array.from(Array(10), (e, i) => {
-          return (
-            <div key={i} className={'select-type__box'}>
-              <div>
-                <div className={'select-type__box--size'}>US 4</div>
-                <div className={'select-type__box--price'}>4500.-</div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      {/*<SizePool />*/}
+      <AvailableList />
     </div>
   );
 };
