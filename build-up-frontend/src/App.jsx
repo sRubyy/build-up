81515167a17182b22535bf31073fce3287a473db
mon <mon.ppunnavich@gmail.com>
2023-10-30 23:07:11 +0700

    update product controller & frontend for seller

--- build-up-frontend/src/App.jsx
+++ build-up-frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { Outlet } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import Navbar from './components/global/Navbar';
+import { ProductDescription } from './pages/ProductDescription/ProductDescription';
+import { ProductCheckout } from './pages/ProductCheckout/ProductCheckout';
 
 function App() {
   return (
