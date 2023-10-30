4776cf49e7f9b825dd63cc86cb8b9a0f575687f6
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-10-30 13:11:26 +0700

    [GEN-33] create AvailableList.jsx

---
+++ build-up-frontend/src/pages/ProductDescription/components/SizePool.jsx
@@ -0,0 +1,25 @@
+import '../../../scss/product_description/size_selection.scss';
+import React from 'react';
+
+export const SizePool = () => {
+  return (
+    <>
+      <div className={'select-type__second-desc'}>
+        <div className={'select-type__size-desc'}>Size (US Unisex)</div>
+        <div className={'select-type__size-chart'}>Size Chart</div>
+      </div>
+      <div className={'select-type__size-pool'}>
+        {Array.from(Array(10), (e, i) => {
+          return (
+            <div key={i} className={'select-type__box'}>
+              <div>
+                <div className={'select-type__box--size'}>US 4</div>
+                <div className={'select-type__box--price'}>4500.-</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
