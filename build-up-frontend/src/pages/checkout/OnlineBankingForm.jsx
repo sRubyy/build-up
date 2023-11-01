c2ecc3b14d9f229a9fbe689a937cf53996acdda3
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-11-01 15:42:11 +0700

    [GEN-33] create 2 payment forms

---
+++ build-up-frontend/src/pages/checkout/OnlineBankingForm.jsx
@@ -0,0 +1,19 @@
+import '../../scss/checkout/checkout.scss';
+import '../../scss/checkout/checkout_form.scss';
+import HorizontalDropdown from './HorizontalDropdown';
+import InputText from './InputText';
+function OnlineBankingForm() {
+  return (
+    <>
+      <HorizontalDropdown
+        inputName={'Select Bank'}
+        selectList={['Select bank', 'SCB']}
+        selectItem={'Select bank'}
+      />
+      <InputText inputName={'Name'} placeholder={'Enter name'} />
+      <InputText inputName={'Account number'} placeholder={'xxx-xxxxxx'} />
+    </>
+  );
+}
+
+export default OnlineBankingForm;
