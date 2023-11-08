bdefa8d3b3e10487dc647a8e69cb6905f0756122
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-11-08 23:02:45 +0700

    add title text in search result page

--- build-up-frontend/src/pages/search_result/SearchResult.jsx
+++ build-up-frontend/src/pages/search_result/SearchResult.jsx
@@ -1,87 +1,91 @@
-import { useEffect, useState } from "react";
-import ProductModel from "../../models/ProductModel";
-import ItemShowcase from '../search_result/components/itemShowcase'
-import { useLocation } from "react-router-dom";
-import { itemImageMapping } from "../../config/item_image_mapping";
-import '../../scss/search_result/search_result.scss'
-
+import { useEffect, useState } from 'react';
+import ProductModel from '../../models/ProductModel';
+import ItemShowcase from '../search_result/components/itemShowcase';
+import { useLocation } from 'react-router-dom';
+import { itemImageMapping } from '../../config/item_image_mapping';
+import '../../scss/search_result/search_result.scss';
 
 export const SearchResult = () => {
-    const [results, setResults] = useState([]);
-    const [query, setQuery] = useState('');
-    const location = useLocation();
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('');
+  const location = useLocation();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            if (location.state && location.state.data) {
-                setQuery(location.state.data.query);
-            } else {
-                console.error('Data is not available in location state.');
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      if (location.state && location.state.data) {
+        setQuery(location.state.data.query);
+      } else {
+        console.error('Data is not available in location state.');
+      }
+    };
 
-        fetchData();
-    }, [location.state]);
+    fetchData();
+  }, [location.state]);
 
-    useEffect(() => {
-        const fetchResults = async () => {
-            const baseUrl = 'http://localhost:8080/api/product/';
-            let url = ''
-            
-            if(query === ''){
-                url = baseUrl;
-            }else{
-                url = baseUrl + `findByName?name=${query}`;
-            }
+  useEffect(() => {
+    const fetchResults = async () => {
+      const baseUrl = 'http://localhost:8080/api/product/';
+      let url = '';
 
-            try {
-                const response = await fetch(url, {
-                    method: 'GET'
-                });
+      if (query === '') {
+        url = baseUrl;
+      } else {
+        url = baseUrl + `findByName?name=${query}`;
+      }
 
-                if (!response.ok) {
-                    throw new Error('Error found');
-                }
+      try {
+        const response = await fetch(url, {
+          method: 'GET',
+        });
 
-                const responseJson = await response.json();
-
-                const loadedResults = Object.keys(responseJson).map(key => {
-                    const product = new ProductModel(
-                        responseJson[key].name,
-                        responseJson[key].description,
-                        responseJson[key].price,
-                        responseJson[key].type,
-                        responseJson[key].size,
-                        responseJson[key].brandNew
-                    );
-
-                    product.setCreatedAt(responseJson[key].createAt);
-                    product.setPurchaseDate(responseJson[key].purchaseDate);
-                    product.setId(responseJson[key].productId);
-
-                    return product;
-                });
-
-                setResults(loadedResults);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        if(query){
-            fetchResults();
+        if (!response.ok) {
+          throw new Error('Error found');
         }
-    }, [query]);
 
-    return (
-        <div className="product-detail">
-            <div className="showcaselist">
-                {results.map((item, i) => (
-                    <div key={i}>
-                        <ItemShowcase name={item.name} price={item.price} imageUrl={itemImageMapping[item.name].snippetImage}/>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+        const responseJson = await response.json();
+
+        const loadedResults = Object.keys(responseJson).map((key) => {
+          const product = new ProductModel(
+            responseJson[key].name,
+            responseJson[key].description,
+            responseJson[key].price,
+            responseJson[key].type,
+            responseJson[key].size,
+            responseJson[key].brandNew
+          );
+
+          product.setCreatedAt(responseJson[key].createAt);
+          product.setPurchaseDate(responseJson[key].purchaseDate);
+          product.setId(responseJson[key].productId);
+
+          return product;
+        });
+
+        setResults(loadedResults);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (query) {
+      fetchResults();
+    }
+  }, [query]);
+
+  return (
+    <div className="product-detail" style={{ margin: '5% 10%' }}>
+      <div className={'search-title'}>Search results</div>
+      <div className="showcase-list">
+        {results.map((item, i) => (
+          <div key={i}>
+            <ItemShowcase
+              name={item.name}
+              price={item.price}
+              imageUrl={itemImageMapping[item.name].snippetImage}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
