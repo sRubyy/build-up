addd5edb2c15605610eb87447535fb8ed78cb6d0
Twans <82774312+twans99@users.noreply.github.com>
2023-11-08 21:41:53 +0700

    integrate add Credit Card

--- build-up-frontend/src/pages/checkout/components/InputText.jsx
+++ build-up-frontend/src/pages/checkout/components/InputText.jsx
@@ -1,5 +1,5 @@
 import '../../../scss/checkout/input.scss';
-function InputText({ inputName, placeholder }) {
+function InputText({ inputName, placeholder, value, setValue = () => {} }) {
   return (
     <div className={'input-container'}>
       <div className={'input-label'}>{inputName}</div>
@@ -8,6 +8,8 @@ function InputText({ inputName, placeholder }) {
           style={{ all: 'unset', width: '100%' }}
           type={'text'}
           placeholder={placeholder}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
       </div>
     </div>
