938f5876569328f95399b9564b9dbc860fde2019
PoomrapeeWareeboutr <poomrapee.war@student.mahidol.ac.th>
2023-11-25 01:54:47 +0700

    make docker-compose.yaml compatible

--- build-up-backend/src/test/java/com/gemini11/buildupbackend/BuildUpBackendApplicationTests.java
+++ build-up-backend/src/test/java/com/gemini11/buildupbackend/BuildUpBackendApplicationTests.java
@@ -1,13 +1,7 @@
 package com.gemini11.buildupbackend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BuildUpBackendApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
